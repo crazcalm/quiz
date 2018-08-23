a5b3f256862a5955a6326d9af85e4667fdd42bfa
Marcus Willock <crazcalm@gmail.com>
2018-08-23 17:26:40 -0400

    Add ActiveConnections method
    I can now return a list of active connections based on a single timestamp

--- src/data.go
+++ src/data.go
@@ -31,3 +31,92 @@ func (d DataCollection) Less(i, j int) bool {
 
 	return false
 }
+
+/*ActiveConnections algorithm:
+---------------
+
+Given the collection is sorted by start time and time taken (secondary sort),
+Then any start times post input time T can be ignored.
+
+Thus, I can find the first index X of the collection that has a start time
+that starts after input time T.
+
+Then I can iterate from X - 1 down and mark every case Y where the end time
+is after the input time T.
+
+All of the marked items will consist of all the connections that were in
+progress at input Time T.
+*/
+func (d DataCollection) ActiveConnections(timestamp string) (DataCollection, error) {
+	inputTime, err := ParseDate(timestamp)
+	if err != nil {
+		return []Data{}, err
+	}
+
+	return d.activeConnections(inputTime), nil
+}
+
+func (d DataCollection) activeConnections(timestamp time.Time) DataCollection {
+	results := []Data{}
+	startIndex := d.findStartIndex(timestamp)
+
+	for i := startIndex; i > 0; i-- {
+		// An EndTime that ends before the input time starts
+		// means that we have left the range of possible active
+		// connections.
+		if d[i].EndTime.Before(timestamp) {
+			break
+		}
+
+		if d[i].StartTime.Before(timestamp) && compareTime(d[i].EndTime, timestamp) {
+			results = append(results, d[i])
+			continue // Prvents d[i] from statisfying both if statements and, as a result, be double counted
+		}
+
+		if d[i].StartTime.Before(timestamp) && d[i].EndTime.After(timestamp) {
+			results = append(results, d[i])
+		}
+	}
+	return results
+}
+
+/* findStartIndex is used to the best index in which to start the ActiveConnections iterative search.
+
+It does this via a tradtional binary search. However, (due to timestamps not being unique) if the
+passed in timestamp is found, we then need to iterate down the DataCollection to the last instance
+that time stamp.
+
+If the timestamp is not found, we then will return the left index because the timestamp at that
+index will be greater than the timestamp we were looking for.
+*/
+func (d DataCollection) findStartIndex(timestamp time.Time) int {
+	var result int
+	left := 0
+	right := len(d) - 1
+
+	for left <= right {
+		middle := (left + right) / 2
+		if compareTime(d[middle].StartTime, timestamp) {
+			result = middle
+			break
+		}
+
+		if d[middle].StartTime.Before(timestamp) {
+			left = middle + 1
+		} else {
+			right = middle - 1
+		}
+	}
+
+	if result == 0 { // Means that the binary search did not find the result
+		return left
+	}
+
+	// Need to ensure that the found timestamp is the last instance of this timestamp
+	for ; result < len(d); result++ {
+		if d[result].StartTime.After(timestamp) {
+			return result
+		}
+	}
+	return result
+}
--- src/data_test.go
+++ src/data_test.go
@@ -3,8 +3,139 @@ package quiz
 import (
 	"path/filepath"
 	"sort"
+	"strings"
 	"testing"
 )
+
+type testData struct {
+	IP        string
+	EndTime   string
+	TimeTaken int
+}
+
+func TestActiveConnecions(t *testing.T) {
+	tests := []struct {
+		file      string
+		inputTime string
+		expected  []testData
+	}{
+		{
+			filepath.Join("test_data", "log.csv"),
+			"2017-10-23T11:58:00.000",
+			[]testData{},
+		},
+		{
+			filepath.Join("test_data", "log.csv"),
+			"2017-10-23T12:00:00.000",
+			[]testData{
+
+				testData{
+					"1.2.3.24",
+					"2017-10-23T12:00:00.000",
+					200,
+				},
+
+				testData{
+					"1.2.3.15",
+					"2017-10-23T12:00:01.000",
+					1200,
+				},
+
+				testData{
+					"1.2.3.9",
+					"2017-10-23T12:00:00.000",
+					100,
+				},
+
+				testData{
+					"1.2.3.19",
+					"2017-10-23T12:00:00.000",
+					100,
+				},
+
+				testData{
+					"1.2.3.4",
+					"2017-10-23T12:00:00.000",
+					20,
+				},
+
+				testData{
+					"1.2.3.14",
+					"2017-10-23T12:00:00.000",
+					20,
+				},
+			},
+		},
+		{
+			filepath.Join("test_data", "log.csv"),
+			"2017-10-23T12:22:00.000",
+			[]testData{
+				testData{
+					"1.2.3.48",
+					"2017-10-23T12:22:00.000",
+					600,
+				},
+				testData{
+					"1.2.3.18",
+					"2017-10-23T12:22:00.000",
+					200,
+				},
+				testData{
+					"1.2.3.8",
+					"2017-10-23T12:22:00.000",
+					200,
+				},
+			},
+		},
+	}
+
+	for index, test := range tests {
+		data, err := ImportLogsFromCSV(test.file)
+		if err != nil {
+			t.Errorf("Case %d: Unexpected Error: %s", index, err.Error())
+		}
+		sort.Sort(data)
+
+		//Start of test
+		activeConnections, err := data.ActiveConnections(test.inputTime)
+		if err != nil {
+			t.Fail()
+		}
+		sort.Sort(activeConnections)
+
+		if len(activeConnections) != len(test.expected) {
+
+			t.Log("input time: ", test.inputTime)
+
+			t.Log("Logs:")
+			for _, i := range data {
+				t.Log(i)
+			}
+
+			t.Log("active connections")
+			for _, i := range activeConnections {
+				t.Log(i)
+			}
+			t.Errorf("Case %d: Expected %d elements, but got %d", index, len(test.expected), len(activeConnections))
+			continue
+		}
+
+		for i, conn := range activeConnections {
+			if !strings.EqualFold(conn.IP, test.expected[i].IP) {
+				t.Errorf("Case %d: Expected index %d to have an IP name of %s, but got %s", index, i, test.expected[i].IP, conn.IP)
+			}
+
+			if !strings.EqualFold(conn.OriginalTimeFormat, test.expected[i].EndTime) {
+				t.Errorf("Case %d: Expected index %d to have a EndTime string of %s, but got %s", index, i, test.expected[i].EndTime, conn.OriginalTimeFormat)
+			}
+
+			if conn.TimeTaken != test.expected[i].TimeTaken {
+				t.Errorf("Case %d: Expected index %d to have a TimeTaken value of %d, but got %d", index, i, test.expected[i].TimeTaken, conn.TimeTaken)
+			}
+		}
+
+	}
+}
 
 func TestSortDataCollection(t *testing.T) {
 	tests := []struct {
